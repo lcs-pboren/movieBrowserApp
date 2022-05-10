94a4b31aea09e3cf866121c622d4c1ad0425b049
Patrick Boren <Pboren@lcs.on.ca>
2022-05-10 09:32:37 -0400

    Initial Commit

---
+++ movieBrowserApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  movieBrowserApp
+//
+//  Created by Patrick Boren on 2022-05-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ movieBrowserApp/movieBrowserAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  movieBrowserAppApp.swift
+//  movieBrowserApp
+//
+//  Created by Patrick Boren on 2022-05-10.
+//
+
+import SwiftUI
+
+@main
+struct movieBrowserAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
